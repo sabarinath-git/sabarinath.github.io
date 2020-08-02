891548c66f21d681a14734e949f4094ba5c10358
sabarinath-git <69119629+sabarinath-git@users.noreply.github.com>
2020-08-02 12:31:03 -0700

    Update agehist.js
    edited csv file path

--- scripts/agehist.js
+++ scripts/agehist.js
@@ -28,7 +28,7 @@ function draw_agehist() {
 
 	//d3.csv("data/2c_titanic_agebin_surv.csv", function(error, data) {
 	//d3.csv("https://github.com/sabarinath-git/titanic_d3js/tree/master/data/2c_titanic_agebin_surv.csv", function(error, data) {	
-	d3.csv("2c_titanic_agebin_surv.csv", function(error, data) {	
+	d3.csv("/2c_titanic_agebin_surv.csv", function(error, data) {	
         if (error) {
             throw error;
         }
